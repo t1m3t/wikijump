ddc36a23b20cf8238d5aa18fadac0deb389744a2
Tim <toqul4j@gmail.com>
2021-07-31 20:15:07 -0700

    add jumps to random wiki pages

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const url = "https://en.wikipedia.org/wiki/Faker_(gamer)"
+let url = "https://en.wikipedia.org/wiki/Hasan_Piker";
+
+const wikiLink = /^\/wiki\/(?!.*:).*/;
+
+const linkJumps = 12;
 
 async function scrapeData() {
   try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-    const title = $("h1");
-    console.log(title.text());
+    for (let i = 0; i < linkJumps; i++) {
+      const { data } = await axios.get(url);
+      const $ = cheerio.load(data);
+
+      const links = $("#bodyContent a");
+
+      let totalLinks = [];
+      links.each((_, el) => {
+        const link = $(el).attr("href");
+        if (wikiLink.test(link)) {
+          totalLinks.push(link);
+        }
+      });
+
+      console.log($("h1").text());
+      console.log(` - ${url}`);
+
+      url = `https://wikipedia.org${randomLink(totalLinks)}`;
+    } 
   } catch (err) {
     console.log(err);
   }
+}
+
+function randomLink(links) {
+  return links[Math.floor(Math.random() * links.length)];
 }
 
 scrapeData();
